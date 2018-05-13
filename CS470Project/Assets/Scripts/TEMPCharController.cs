de468ab5a496a40175068d02dfcb4e10446b8e20
mwono <michaelwono@gmail.com>
2018-05-12 19:58:21 -0700

    Added folders + Camera Test
    Added some folders for organizatio purposes.
    In scene MWTestScene you can see the half-monochrome-half-color camera concept. Everything in sprites and scripts is temporary for the purposes of testing the camera and should probably be deleted later.

---
+++ CS470Project/Assets/Scripts/TEMPCharController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEMPCharController : MonoBehaviour {
+    Rigidbody2D rb;
+    //DELETE THIS LATER
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * 2, Input.GetAxis("Vertical") * 2);
+	}
+}
