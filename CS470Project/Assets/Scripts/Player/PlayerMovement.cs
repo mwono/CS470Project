2ed50fe388de02f248fd4dfa7bf05835b2a613f9
Sean Castillo <seanfcastillo@gmail.com>
2018-05-19 18:00:52 -0700

    added character controller and gameplay scripts.

---
+++ CS470Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public GameObject gameOverCanvas;
+
+    float maxJumpHeight = 4f;
+    float groundHeight;
+    Vector3 groundPos;
+    float jumpSpeed = 8.0f;
+    float fallSpeed = 11.0f;
+    public bool inputJump = false;
+    public bool grounded = true;
+    public Animator playerAnim;
+
+    void Start()
+    {
+        groundPos = transform.position;
+        groundHeight = transform.position.y;
+        maxJumpHeight = transform.position.y + maxJumpHeight;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                groundPos = transform.position;
+                inputJump = true;
+                playerAnim.SetBool("Jump", true);
+                StartCoroutine("Jump");
+            }
+        }
+        if (transform.position == groundPos)
+            grounded = true;
+        else
+            grounded = false;
+
+
+    }
+
+    IEnumerator Jump()
+    {
+        while (true)
+        {
+            if (transform.position.y >= maxJumpHeight)
+                inputJump = false;
+            if (inputJump)
+                transform.Translate(Vector3.up * jumpSpeed * Time.smoothDeltaTime);
+            else if (!inputJump)
+            {
+                transform.Translate(Vector3.down * fallSpeed * Time.smoothDeltaTime);
+                if (transform.position.y < groundPos.y)
+                {
+                    transform.position = groundPos;
+                    playerAnim.SetBool("Jump", false);
+                    StopAllCoroutines();
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // check if we die
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            GameEvents.TriggerPlayerDie();
+            gameOverCanvas.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ CS470Project/Assets/Scripts/ScrollingBackground/CleanupObstacles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanupObstacles : MonoBehaviour {
+
+    public BackgroundScroll bgScrollScript;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            bgScrollScript.RemoveObstacle(collision.gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+}
