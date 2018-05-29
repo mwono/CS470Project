17a68ded7ac9e4a1734faa1c56a03cb408a607b5
Sean Castillo <seanfcastillo@gmail.com>
2018-05-29 03:23:06 -0700

    added John's menu stuff! his github is bugged so he sent me the files

---
+++ CS470Project/Assets/UIPause.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIPause : MonoBehaviour
+{
+    public Transform canvas;
+    public Animator pauseMenu;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.StartPlayback();
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                
+                //pauseMenu.StartPlayback();
+
+                Time.timeScale = 0;
+            }
+            else        //else, pause menu is already active
+            {
+                Time.timeScale = 1;
+                canvas.gameObject.SetActive(false);
+
+            }
+        }
+
+    }
+
+
+    public void ResumeGame()
+    {
+        canvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
