099fade9ff41c43835a9292774c6f75c918c7a40
mwono <michaelwono@gmail.com>
2018-05-31 20:02:09 -0700

    scored boost
    boosted score from jumping from 200 --> 1000. 200 too unnoticeable

--- CS470Project/Assets/Scripts/Player/Score.cs
+++ CS470Project/Assets/Scripts/Player/Score.cs
@@ -13,21 +13,21 @@ public class Score : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         timeToStart -= Time.deltaTime;
-
+    
         if (timeToStart <= 0f && !GameOver.activeInHierarchy)
         {
             sc = int.Parse(score.text);
-            sc += Mathf.CeilToInt(Time.deltaTime);
-            score.text = sc.ToString();
+            sc++;
         }
-	}
+        score.text = sc.ToString();
+    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Obstacle")
         {
             sc = int.Parse(score.text);
-            sc += 200;
+            sc += 1000;
             score.text = sc.ToString();
         }
     }
